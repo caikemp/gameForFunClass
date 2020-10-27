b1adc2bf21ab5409e9a24fd484e4a8f64896868a
Danilo Baraldi <45182291+danilobaraldi@users.noreply.github.com>
2020-10-27 20:59:28 -0300

    Applied key and valors (#8)

--- app/Http/Parse/Suntech319.php
+++ app/Http/Parse/Suntech319.php
@@ -7,8 +7,20 @@ class Suntech319
     private $deviceId;
     private $model;
     private $date;
+    private $cell;
     private $lat;
     private $lon;
+    private $speed;
+    private $course;
+    private $satt;
+    private $fixGps;
+    private $distance;
+    private $voltage;
+    private $inputOutput;
+    private $mode;
+    private $msgNum;
+    private $hourMeter;
+    private $battery;
 
     public function setHeader($header)
     {
@@ -30,9 +42,14 @@ class Suntech319
         return $this->deviceId;
     }
 
-    public function setDate($date)
+    public function setModel($model)
     {
-        $this->date = $date;
+        $this->model = $model;
+    }
+
+    public function getModel()
+    {
+        return $this->model;
     }
 
     public function getDate()
@@ -40,12 +57,161 @@ class Suntech319
         return $this->date;
     }
 
+    public function setDate($date)
+    {
+        $this->date = $date;
+    }
+
+    public function getCell()
+    {
+        return $this->cell;
+    }
+
+    public function setCell($cell)
+    {
+        $this->cell = $cell;
+    }
+
+    public function getLat()
+    {
+        return $this->lat;
+    }
+
+    public function setLat($lat)
+    {
+        $this->lat = $lat;
+    }
+
+    public function getLon()
+    {
+        return $this->lon;
+    }
+    public function setLon($lon)
+    {
+        $this->lon = $lon;
+    }
+
+    public function getSpeed()
+    {
+        return $this->speed;
+    }
+    public function setSpeed($speed)
+    {
+        $this->speed = $speed;
+    }
+
+    public function getCourse()
+    {
+        return $this->course;
+    }
+    public function setCourse($course)
+    {
+        $this->course = $course;
+    }
+
+    public function getSatt()
+    {
+        return $this->satt;
+    }
+    public function setSatt($satt)
+    {
+        $this->satt = $satt;
+    }
+
+    public function getFixGps()
+    {
+        return $this->fixGps;
+    }
+    public function setFixGps($fixGps)
+    {
+        $this->fixGps = $fixGps;
+    }
+
+    public function getDistance()
+    {
+        return $this->distance;
+    }
+    public function setDistance($distance)
+    {
+        $this->distance = $distance;
+    }
+
+    public function getVoltage()
+    {
+        return $this->voltage;
+    }
+    public function setVoltage($voltage)
+    {
+        $this->voltage = $voltage;
+    }
+
+    public function getInputOutput()
+    {
+        return $this->inputOutput;
+    }
+    public function setInputOutput($inputOutput)
+    {
+        $this->inputOutput = $inputOutput;
+    }
+
+    public function getMode()
+    {
+        return $this->mode;
+    }
+    public function setMode($mode)
+    {
+        $this->mode = $mode;
+    }
+
+    public function getMsgNum()
+    {
+        return $this->msgNum;
+    }
+    public function setMsgNum($msgNum)
+    {
+        $this->msgNum = $msgNum;
+    }
+
+    public function getHourMeter()
+    {
+        return $this->hourMeter;
+    }
+    public function setHourMeter($hourMeter)
+    {
+        $this->hourMeter = $hourMeter;
+    }
+
+    public function getBatttery()
+    {
+        return $this->battery;
+    }
+    public function setBattery($battery)
+    {
+        $this->battery = $battery;
+    }
+
+
     public function toArray()
     {
         return [
             'header' => $this->header,
+            'model' => $this->model,
             'deviceId' => $this->deviceId,
             'date' => $this->date,
+            'cell' => $this->cell,
+            'lat' => $this->lat,
+            'lon' => $this->lon,
+            'speed' => $this->speed,
+            'course' => $this->course,
+            'satt' => $this->satt,
+            'fixGps' => $this->fixGps,
+            'distance' => $this->distance,
+            'voltage' => $this->voltage,
+            'inputOutput' => $this->inputOutput,
+            'mode' => $this->mode,
+            'msgNum' => $this->msgNum,
+            'hourMeter' => $this->hourMeter,
+            'battery' =>$this->battery
         ];
 
     }
